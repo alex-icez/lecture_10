19a530cd2ff207e2eda70559d395e8306b6523dd
alex-icez <alex-icex@mail.ru>
2015-11-19 20:44:43 +0300

    mess

--- src/lecture_10/Sample4.java
+++ src/lecture_10/Sample4.java
@@ -1,5 +1,6 @@
 package lecture_10;
 
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -9,9 +10,10 @@ public class Sample4 {
 
 	public static void main(String[] args) throws IOException {
 		FileOutputStream out = new FileOutputStream("test");
+		BufferedOutputStream bout = new BufferedOutputStream(out);
 		byte arr[] = "qwerty on".getBytes();
-		out.write(arr);
-		out.close();
+		bout.write(arr);
+		bout.close();
 		
 		
 		FileInputStream in = new FileInputStream("test");

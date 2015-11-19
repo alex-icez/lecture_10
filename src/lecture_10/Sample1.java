536936ba4e623aa9410d713dded264ae72491fb9
alex-icez <alex-icex@mail.ru>
2015-11-19 18:26:02 +0300

    mess

--- src/lecture_10/Sample1.java
+++ src/lecture_10/Sample1.java
@@ -4,7 +4,11 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 		String sample = "abbaa";
-		String text = "aabababababa ababab abbabbbabab abbaa bbabbaa";
+		String text = "aabababababa ababab "
+				+ "abbabbbabab abbaa bbabbaa";
+		int pos;
+		
+		
 		System.out.println(text.indexOf(sample));
 
 	}

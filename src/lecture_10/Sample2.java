67328b60b4176690bcf9a20c1cbcb5e2782a206b
alex-icez <alex-icex@mail.ru>
2015-11-19 18:33:06 +0300

    mess

--- src/lecture_10/Sample2.java
+++ src/lecture_10/Sample2.java
@@ -1,11 +1,20 @@
 package lecture_10;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class Sample2 {
 
 	public static void main(String[] args) {
 		LinkedList<Integer> l = new LinkedList<>();
 		ArrayList<Integer> la = new ArrayList<>();
-
+		
+		HashSet<Integer> hs = new HashSet<>();
+		hs.add(123);
+		hs.contains(123);
+		
+		
 	}
 
 }

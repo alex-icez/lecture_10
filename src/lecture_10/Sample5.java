7667291b37c9e9a0ae22ffe12e290e121d4fb848
alex-icez <alex-icex@mail.ru>
2015-11-19 20:59:02 +0300

    mess

--- src/lecture_10/Sample5.java
+++ src/lecture_10/Sample5.java
@@ -1,11 +1,15 @@
 package lecture_10;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 public class Sample5 {
-	public static void main(String args[]) {
-		ObjectOutputStream out = new ObjectOutputStream(new FileInputStream("out"));
-		
+	public static void main(String args[]) throws FileNotFoundException, IOException {
+		ObjectOutputStream out = new ObjectOutputStream
+				(new FileOutputStream("out"));
+		out.writeByte(123);
 	}
 }

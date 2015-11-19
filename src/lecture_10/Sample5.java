90a20fd12a5f86e935eed0d5388892318a32a6cc
alex-icez <alex-icex@mail.ru>
2015-11-19 20:58:30 +0300

    mess

--- src/lecture_10/Sample5.java
+++ src/lecture_10/Sample5.java
@@ -1,8 +1,11 @@
 package lecture_10;
 
+import java.io.FileInputStream;
+import java.io.ObjectOutputStream;
+
 public class Sample5 {
 	public static void main(String args[]) {
-		
+		ObjectOutputStream out = new ObjectOutputStream(new FileInputStream("out"));
 		
 	}
 }

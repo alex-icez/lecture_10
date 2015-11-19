8f445b04d68dc53bb249ccc5e8183cb3174dc1c9
alex-icez <alex-icex@mail.ru>
2015-11-19 18:28:12 +0300

    mess

--- src/lecture_10/Sample1.java
+++ src/lecture_10/Sample1.java
@@ -4,9 +4,9 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 		String sample = "abbaa";
-		String text = "aabababababa ababab "
+		String text = "abbaa aabababababa ababab "
 				+ "abbabbbabab abbaa bbabbaa";
-		int pos = 0;
+		int pos = -1;
 		while(true) {
 			pos = text.indexOf(sample, pos + 1);
 			if (pos < 0) 

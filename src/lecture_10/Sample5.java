1fb180e6ef13166a1821524abf6dc25bc0e150a3
alex-icez <alex-icex@mail.ru>
2015-11-19 21:01:11 +0300

    mess

--- src/lecture_10/Sample5.java
+++ src/lecture_10/Sample5.java
@@ -6,6 +6,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
+class Point {
+	int x;
+	int y;
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
 public class Sample5 {
 	public static void main(String args[]) throws FileNotFoundException, IOException {
 		ObjectOutputStream out = new ObjectOutputStream

6f13f8b06432fb3d4b00ea9689bf3f982290c3f5
alex-icez <alex-icex@mail.ru>
2015-11-19 18:25:29 +0300

    mess

---
+++ src/lecture_10/Sample1.java
@@ -0,0 +1,12 @@
+package lecture_10;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		String sample = "abbaa";
+		String text = "aabababababa ababab abbabbbabab abbaa bbabbaa";
+		System.out.println(text.indexOf(sample));
+
+	}
+
+}

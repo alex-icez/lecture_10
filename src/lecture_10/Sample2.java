4eb358f4bcca5e3bea0ba9735c93cb2a6379fbfe
alex-icez <alex-icex@mail.ru>
2015-11-19 18:31:29 +0300

    mess

--- src/lecture_10/Sample2.java
+++ src/lecture_10/Sample2.java
@@ -3,7 +3,8 @@ package lecture_10;
 public class Sample2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LinkedList<Integer> l = new LinkedList<>();
+		ArrayList<Integer> la = new ArrayList<>();
 
 	}
 
